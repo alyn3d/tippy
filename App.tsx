913adc38d56aeb098da909a5994ea49a47a74fa7
Alin Ion <alyn3d@gmail.com>
2022-07-21 18:00:12 +0300

    feat: :sparkles: V3.0.2
    Android navigation bar now properly changes color scheme according to system theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
 import { ApplicationProvider } from '@ui-kitten/components';
 import { Home } from './src/screens/Home';
 
 export default function App() {
   const colorScheme = useColorScheme();
   //console.log(colorScheme);
+  useEffect(() => {
+    const setNavBarColorScheme = async () => {
+      if (colorScheme === 'light') {
+        await NavigationBar.setBackgroundColorAsync("white");
+        await NavigationBar.setButtonStyleAsync("light");
+      } else {
+        await NavigationBar.setBackgroundColorAsync("#222B45");
+        await NavigationBar.setButtonStyleAsync("dark");
+      }
+    }
+
+    setNavBarColorScheme();
+  }, []);
 
   return (
       <ApplicationProvider {...eva} theme={colorScheme === 'light' ? eva.light : eva.dark}>
